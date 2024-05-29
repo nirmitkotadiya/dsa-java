99826bfbc36b3168df7677d18bdbc9ac08087a89
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-29 19:56:16 +0530

    06 file updated
    06 file updated

--- 06_Variables_Data_Types/JavaBasics.java
+++ 06_Variables_Data_Types/JavaBasics.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class JavaBasics {
     public static void main(String[] args) {
         // System.out.print("Hello World!");
@@ -17,14 +19,66 @@ public class JavaBasics {
         // System.out.println("**");
         // System.out.println("*");
 
-        int a = 10;
-        int b = 5;
-        System.out.println(a);
-        System.out.println(b);
-        String name = "Tony Stark";
-        System.out.println(name);
-        a = b;
-        System.out.println(a);
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a);
+        // System.out.println(b);
+        // String name = "Tony Stark";
+        // System.out.println(name);
+        // a = b;
+        // System.out.println(a);
+
+        // Data Types in Java
+        // -> Java is Typed Language
+
+        // byte b = 8;
+        // System.out.println(b);
+        // char ch = 'a';
+        // System.out.println(ch);
+        // boolean var = false;
+        // float price = 10.5f;
+        // int number = 25;
+        // long
+        // double
+        // short n = 240;
+
+        // sum of a & b
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println("sum " + sum);
+
+        // Input in java
+        // Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+        // int number = sc.nextInt();
+        // System.out.println(number);
+        // byte byteNumber = sc.nextByte();
+        // System.out.println(byteNumber);
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+        // double doubleNumber = sc.nextDouble();
+        // System.out.println(doubleNumber);
+        // boolean bool = sc.nextBoolean();
+        // System.out.println(bool); // enter True convert true , enter False convert
+        // false
+        // short shortNumber = sc.nextShort();
+        // System.out.println(shortNumber);
+        // long longNumber = sc.nextLong();
+        // System.out.println(longNumber);
+
+        // sum of a and b
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter A ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter B ");
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("Sum = " + sum);
 
     }
 }
