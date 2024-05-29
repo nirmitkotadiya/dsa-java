9551cc6644632a487a9653d0f2ad717ddb9e3a9e
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-29 17:09:14 +0530

    06 file added
    06 file added

---
+++ 06_Variables_Data_Types/JavaBasics.java
@@ -0,0 +1,32 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        // System.out.print("Hello World!");
+        // print function
+        // ; statement terminator
+
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+
+        // System.out.print("Hello World!\n");
+        // System.out.print("Hello World!\n");
+        // System.out.print("Hello World!");
+
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Tony Stark";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+
+    }
+}
+
+// boilerplate code
