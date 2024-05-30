488bd08dee09c885cdf24751bee959f6dd4b897f
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-30 11:59:38 +0530

    08 file added
    08 file added

---
+++ 08_Conditional_Statements/Condition.java
@@ -0,0 +1,139 @@
+import java.util.*;
+
+public class Condition {
+    public static void main(String[] args) {
+        // if else Statements
+        // int age = 22;
+        // if (age >= 18) {
+        // System.out.println("vote , drive");
+        // } else {
+        // System.out.println("not adult");
+        // }
+
+        // Print the largest of 2
+        // int a = 10, b = 3;
+        // if (a >= b) {
+        // System.out.println("a = " + a + " is max");
+        // } else {
+        // System.out.println("b = " + b + " is max");
+        // }
+
+        // Print if a number is Odd or Even
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number");
+        // int num = sc.nextInt();
+        // if (num % 2 == 0) {
+        // System.out.println(num + " is even.");
+        // } else {
+        // System.out.println(num + " is odd.");
+        // }
+
+        // else if Statements
+        // int age = 13;
+        // if (age >= 18) {
+        // System.out.println("adult");
+        // } else if (age >= 13 && age < 18) {
+        // System.out.println("teenager");
+        // } else {
+        // System.out.println("child");
+        // }
+
+        // Income Tax Calculator
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your Income");
+        // int income = sc.nextInt();
+        // int tax;
+        // if (income < 500000) {
+        // tax = 0;
+        // } else if (income >= 500000 && income < 1000000) {
+        // tax = (int) (income * 0.2);
+        // } else {
+        // tax = (int) (income * 0.3);
+        // }
+        // System.out.println("your tax is : " + tax);
+
+        // Print the Largest of 3
+        // int a = 1, b = 4, c = 2;
+        // if (a >= b && a >= c) {
+        // System.out.println("a = " + a + " is max");
+        // } else if (b >= c) {
+        // System.out.println("b = " + b + " is max");
+        // } else {
+        // System.out.println("c = " + c + " is max");
+        // }
+
+        // ternary Operator
+        // int largest = (5 > 3) ? 5 : 3;
+        // System.out.println(largest);
+
+        // String type = (5 % 2 == 0) ? "even" : "odd";
+        // System.out.println(type);
+
+        // Check if a Student will Pass or Fail
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter marks: ");
+        // int marks = sc.nextInt();
+        // String result = (marks >= 33) ? "pass" : "fail";
+        // System.out.println(result);
+
+        // switch Statement
+        // int number = 2;
+        // switch (number) {
+        // case 1:
+        // System.out.println("Case 1");
+        // break;
+        // case 2:
+        // System.out.println("Case 2");
+        // break;
+        // default:
+        // System.out.println("Default Case");
+        // }
+
+        // Calculator
+
+        // + - * / %
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a");
+        // int a = sc.nextInt();
+        // System.out.println("Enter b");
+        // int b = sc.nextInt();
+        // System.out.println("Enter oprator");
+        // char oper = sc.next().charAt(0);
+        // switch (oper) {
+        // case '+':
+        // System.out.println("add = " + (a + b));
+        // break;
+        // case '-':
+        // System.out.println("sub = " + (a - b));
+        // break;
+        // case '*':
+        // System.out.println("mul = " + (a * b));
+        // break;
+        // case '/':
+        // System.out.println("div = " + (a / b));
+        // break;
+        // case '%':
+        // System.out.println("modulo = " + (a % b));
+        // break;
+
+        // default:
+        // System.out.println("Enter Vaild oprator");
+        // break;
+        // }
+
+        // leap year or not
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter year");
+        // int year = sc.nextInt();
+        // boolean x = year % 4 == 0;
+        // boolean y = year % 100 != 0;
+        // boolean z = year % 100 == 0 && year % 400 == 0;
+
+        // if (x && (y || z)) {
+        // System.out.println(year + " is a leap year");
+        // } else {
+        // System.out.println(year + " is not a leap year");
+        // }
+
+    }
+}
