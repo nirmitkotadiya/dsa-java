ee175310ac59073608ba2bd19ed2af9ccb797cfe
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-30 10:34:30 +0530

    07 file added
    07 file added

---
+++ 07_Operators/Operators.java
@@ -0,0 +1,74 @@
+public class Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators (Binary / Unary)
+        // Binary
+        // int a = 10, b = 5;
+        // System.out.println("Addition= " + (a + b));
+        // System.out.println("Subtraction= " + (a - b));
+        // System.out.println("Multiplication= " + (a * b));
+        // System.out.println("Division= " + (a / b));
+        // System.out.println("Modulo= " + (a % b));
+
+        // Unary
+        // pre increment
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a + " " + b);
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a + " " + b);
+
+        // post increment
+        // int a = 10;
+        // int b = a++;
+        // System.out.println(a + " " + b);
+        // int a = 10;
+        // int b = a--;
+        // System.out.println(a + " " + b);
+
+        // Relational Operators
+        // int a = 10, b = 5;
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+        // System.out.println(a <= b);
+        // System.out.println(a >= b);
+
+        // Logical Operators
+        // System.out.println((3 > 2) && (5 > 0));
+        // System.out.println((3 > 2) && (5 < 0));
+        // System.out.println((3 < 2) && (5 > 0));
+        // System.out.println((3 < 2) && (5 < 0));
+
+        // System.out.println((3 > 2) || (5 > 0));
+        // System.out.println((3 > 2) || (5 < 0));
+        // System.out.println((3 < 2) || (5 > 0));
+        // System.out.println((3 < 2) || (5 < 0));
+
+        // System.out.println(!(3 > 2));
+        // System.out.println(!(3 < 2));
+
+        // Assignment Operators
+        // int a = 10;
+        // // a = a + 10;
+        // a += 10;
+        // int b = 5;
+        // // b = b - 5;
+        // b -= 5;
+        // System.out.println(a + " " + b);
+
+        // int c = 5;
+        // c *= 5; // c = c * 5
+        // System.out.println(c);
+
+        // int d = 5;
+        // d /= 5; // d = d / 5
+        // System.out.println(d);
+
+        // int e = 5;
+        // e %= 5; // e = e % 5
+        // System.out.println(e);
+
+    }
+}
