094d890ba63147661c3ae9df2c4109dd1c20d2bf
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-30 19:03:43 +0530

    10 file added
    10 file added

---
+++ 10_Patterns_(Part_I)/Patterns.java
@@ -0,0 +1,42 @@
+
+public class Patterns {
+    public static void main(String[] args) {
+
+        // Print Star Pattern
+        // for (int line = 1; line <= 4; line++) {
+        // // one line
+        // for (int star = 1; star <= line; star++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Print Inverted-Star Pattern
+        // int n = 5;
+        // for (int line = 1; line <= n; line++) {
+        // for (int star = 1; star <= n - line + 1; star++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Print Half-Pyramid Pattern
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // Print Character Pattern
+        // char ch = 'A';
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(ch);
+        // ch++;
+        // }
+        // System.out.println();
+        // }
+
+    }
+}
