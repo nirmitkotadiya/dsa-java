dbf66781362d540754322c731ebbf73c841307d0
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-05-30 17:34:58 +0530

    09 file added
    09 file added

---
+++ 09_Loops_(Flow_Control)/Loops.java
@@ -0,0 +1,152 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String[] args) {
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+
+        // while Loop
+        // int i = 1;
+        // while (i <= 100) {
+        // System.out.println(i + " Hello World");
+        // i++;
+        // }
+
+        // print number from 1 to 10
+        // int num = 1;
+        // while (num <= 10) {
+        // System.out.print(num + " ");
+        // num++;
+        // }
+
+        // print number from 1 to n
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number ");
+        // int num = sc.nextInt();
+        // int i = 1;
+        // while (i <= num) {
+        // System.out.print(i + " ");
+        // i++;
+        // }
+
+        // print sum of first n natural numbers
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number ");
+        // int num = sc.nextInt();
+        // int i = 1;
+        // int sum = 0;
+        // while (i <= num) {
+        // sum = sum + i;
+        // i++;
+        // }
+        // System.out.println("sum= " + sum);
+
+        // for Loop
+        // for (int i = 1; i <= 5; i++) {
+        // System.out.println(i + " Hello World");
+        // }
+
+        // print SQUARE pattern
+        // for (int line = 1; line <= 4; line++) {
+        // System.out.println("****");
+        // }
+
+        // int line = 1;
+        // while (line <= 4) {
+        // System.out.println("****");
+        // line++;
+        // }
+
+        // print Reverse of a Number
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number ");
+        // int num = sc.nextInt();
+
+        // while (num > 0) {
+        // int lastDigit = num % 10;
+        // System.out.print(lastDigit);
+        // num = num / 10;
+        // }
+
+        // Reverse the given number
+        // int num = 123;
+        // int rev = 0;
+        // int lastDigit = num;
+        // while (num > 0) {
+        // lastDigit = num % 10;
+        // rev = (rev * 10) + lastDigit;
+        // num /= 10;
+        // }
+        // System.out.println(rev);
+
+        // do while Loop
+        // int counter = 1;
+        // do {
+        // System.out.println(counter + " Hello World");
+        // counter++;
+        // } while (counter <= 5);
+
+        // Break Statement
+        // for (int i = 1; i <= 5; i++) {
+        // if (i == 3) {
+        // break;
+        // }
+        // System.out.println(i);
+        // }
+
+        // Keep entering numbers till user enters a multiple of 10
+        // Scanner sc = new Scanner(System.in);
+        // do {
+        // System.out.println("Enter Number");
+        // int num = sc.nextInt();
+        // if (num % 10 == 0) {
+        // break;
+        // }
+        // System.out.println(num);
+        // } while (true);
+
+        // Continue Statement
+        // for (int i = 1; i <= 5; i++) {
+        // if (i == 3) {
+        // continue;
+        // }
+        // System.out.print(i + " ");
+        // }
+
+        // Display all numbers entered by user except multiples of 10
+        // do {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number");
+        // int num = sc.nextInt();
+        // if (num % 10 == 0) {
+        // continue;
+        // }
+        // System.out.println(num);
+        // } while (true);
+
+        // Check if a number is prime or not
+        // prime : 2, 3, 5, 7, 11
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Number");
+        // int num = sc.nextInt();
+        // boolean isPrime = true;
+        // if (num == 2) {
+        // System.out.println("2 is a prime number.");
+        // } else {
+        // for (int i = 2; i <= Math.sqrt(num); i++) {
+        // if (num % i == 0) {
+        // isPrime = false;
+        // }
+        // }
+        // if (isPrime == true) {
+        // System.out.println(num + " is a prime number.");
+        // } else {
+        // System.out.println(num + " is not a prime number.");
+        // }
+        // }
+
+    }
+}
